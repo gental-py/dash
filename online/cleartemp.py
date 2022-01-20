fdbbae486616ece33743d545b51f10778eaf5fe0
Gental <43147008+gental-py@users.noreply.github.com>
2022-01-20 10:28:50 +0100

    Indexes fix

--- online/cleartemp.py
+++ online/cleartemp.py
@@ -62,11 +62,11 @@ for root, dirs, files in os.walk(temp_folder_path):
         folderfile_count += 1
         try:
             os.remove(folder_file_path)
-            print(f"{grn}[{tmpfile_count}] Deleted file: \"{folder_file_path}\"{end}")
+            print(f"{grn}[{folderfile_count}] Deleted file: \"{folder_file_path}\"{end}")
             folder_succes += 1
 
         except:
-            print(f"{red}[{tmpfile_count}] Error: cannot delete file: \"{folder_file_path}\" {end}")
+            print(f"{red}[{folderfile_count}] Error: cannot delete file: \"{folder_file_path}\" {end}")
             folder_errors += 1
 
 
@@ -76,19 +76,21 @@ print(f"{end}Status:  {ppl}Total: {folderfile_count}  {ppl} |  {grn}Succes: {fol
 print("\n" * 5)
 
 dirs  = [f for f in os.listdir(temp_folder_path) if not os.path.isfile(os.path.join(temp_folder_path, f))]
+dir_count = 0
 dir_succes = 0
 dir_errors = 0
 for dir in dirs:
     try:
         os.rmdir(temp_folder_path+dir)
-        print(f"{grn}[{tmpfile_count}] Deleted folder: \"{folder_file_path}\{dir}\"{end}")
+        print(f"{grn}[{dir_count}] Deleted folder: \"{folder_file_path}\{dir}\"{end}")
         dir_succes += 1
 
     except:
-        print(f"{red}[{tmpfile_count}] Cannot delete folder: \"{folder_file_path}\{dir}\"{end}")
+        print(f"{red}[{dir_count}] Cannot delete folder: \"{folder_file_path}\{dir}\"{end}")
         dir_errors += 1
 
 print("\n" * 5)
 print(f"{end}Ended deleting folders.")
 print(f"{end}Status:  {ppl}Total: {dir_errors+dir_succes}  {ppl} |  {grn}Succes: {dir_succes}  {ppl} |  {red} Errors: {dir_errors} {end}")
 print("\n" * 5)
+
